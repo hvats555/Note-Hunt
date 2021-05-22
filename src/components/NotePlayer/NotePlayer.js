cd08d87f716003bb59cd6aa69ea567b98cc21d56
Himalaya vats <hvats.hv@gmail.com>
2021-05-22 13:54:00 +0530

    reverted playing status feature

--- src/components/NotePlayer/NotePlayer.js
+++ src/components/NotePlayer/NotePlayer.js
@@ -3,8 +3,6 @@ import Sound from 'react-sound';
 import './NotePlayer.scss';
 
 export default function NotePlayer(props) {
-    const [mainNotePlayState, setMainNotePlayState] = useState();
-    const [rootNotePlayState, setRootNotePlayState] = useState();
 
     return (
         <div className="note__player">
@@ -12,17 +10,14 @@ export default function NotePlayer(props) {
                 <div onClick={() => {props.notePlayerHandeler(true)}} className="note__player-main-btn">
                     <span className="material-icons">volume_up</span>
                 </div>
-                <p className="note__player-label">{mainNotePlayState ? mainNotePlayState : null}</p>
 
-                <p className="note__player-label">Play the note {mainNotePlayState ? mainNotePlayState : null}</p>
+                <p className="note__player-label">Play the note</p>
 
                 <Sound
                     url= {`./audio/${props.audioName}.wav`}
                     playStatus={props.isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
                     playFromPosition={0}
-                    onLoading={() => (setMainNotePlayState("Loading sound, Please Wait..."))}
-                    onPlaying={() => {setMainNotePlayState("Playing")}}
-                    onFinishedPlaying={() => {props.notePlayerHandeler(false); setMainNotePlayState()}}
+                    onFinishedPlaying={() => {props.notePlayerHandeler(false)}}
                 />
             </div>
             
@@ -30,16 +25,13 @@ export default function NotePlayer(props) {
                 <div onClick={() => {props.rootNotePlayerHandeler(true)}} class="note__player-root-btn">
                     <span class="material-icons">music_note</span>
                 </div>
-                <p class="note__player-label">{rootNotePlayState ? rootNotePlayState : null}</p>
-                <p class="note__player-label">Play root note {rootNotePlayState ? rootNotePlayState : null}</p>
+                <p class="note__player-label">Play root note</p>
 
                 <Sound
                     url= {`./audio/${props.rootAudioName}.wav`}
                     playStatus={props.isRootNotePlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
                     playFromPosition={0}
-                    onLoading={() => (setRootNotePlayState("Loading sound, Please Wait..."))}
-                    onPlaying={() => {setRootNotePlayState("Playing")}}
-                    onFinishedPlaying={() => {props.rootNotePlayerHandeler(false); setRootNotePlayState()}}
+                    onFinishedPlaying={() => {props.rootNotePlayerHandeler(false)}}
                 />
             </div>
         </div>
